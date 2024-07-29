356b5a7854f8de91b7f5b2b07270c4474476ae5b
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-07-30 00:28:55 +0300

    Update
    Updated for style.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
